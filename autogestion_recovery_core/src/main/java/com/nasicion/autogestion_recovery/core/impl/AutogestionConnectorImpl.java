db69cc0bae4ded24b5cafad385309254250bf7f7
nasicion <nasicion@gmail.com>
2013-12-04 19:15:30 -0200

    Added implementation

---
+++ autogestion_recovery_core/src/main/java/com/nasicion/autogestion_recovery/core/impl/AutogestionConnectorImpl.java
@@ -0,0 +1,7 @@
+package com.nasicion.autogestion_recovery.core.impl;
+
+import com.nasicion.autogestion_recovery.core.AutogestionConnector;
+
+public class AutogestionConnectorImpl implements AutogestionConnector {
+
+}
